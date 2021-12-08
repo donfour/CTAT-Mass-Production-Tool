e6e0e7174d86aef7aee3efe4b5fbcebe99e2af26
Donovan So <donovanso@yahoo.com.hk>
2021-12-07 22:03:24 -0500

    Updates

---
+++ deploy.js
@@ -0,0 +1,6 @@
+const ghpages = require('gh-pages');
+
+ghpages.publish('.', {
+  repo: 'https://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@github.com/donfour/CTAT-Mass-Production-Tool.git',
+  // silent: true
+}, console.log);
